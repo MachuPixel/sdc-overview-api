4c070d15aa1918bb630c417b11683c17975021a1
Amelia LI <jkramelia8872@gmail.com>
2023-08-30 16:14:52 -0400

    changing back to PostgresSQL

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ const express = require('express');
 const app = express();
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening at port ${process.env.PORT}...`);
+app.listen(process.env.PORT1, () => {
+  console.log(`Listening at port ${process.env.PORT1}...`);
 });
