7b9f591297e4586917ac9f082718f07473c8393a
Amelia LI <jkramelia8872@gmail.com>
2023-09-05 18:52:27 -0400

    change configuredb.sh

--- bin/configuredb.sh
+++ bin/configuredb.sh
@@ -1,27 +1,13 @@
-# #!/bin/bash
-
-# database="overview"
-
-# echo "Configuring database: $database"
-
-# dropdb -U node_user overview
-# createdb -U node_user overview
-
-# psql -U node_user overview < ./server/database/schema.sql
-
-
-# echo "$database configured"
-
-
 #!/bin/bash
 
 database="overview"
 
 echo "Configuring database: $database"
 
-# Connect to the Dockerized PostgreSQL container and execute the commands
-docker exec -it postgresql dropdb -U node_user $database
-docker exec -it postgresql createdb -U node_user $database
-docker exec -it postgresql psql -U node_user $database < ./server/database/schema.sql
+dropdb -U node_user overview
+createdb -U node_user overview
+
+psql -U node_user overview < ./server/database/schema.sql
+
 
 echo "$database configured"
