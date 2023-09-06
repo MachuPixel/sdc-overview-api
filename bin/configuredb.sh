236c6ea4b8320a2738b2c918d2f2e31e4da0f0f5
Amelia LI <jkramelia8872@gmail.com>
2023-09-05 23:23:25 -0400

    docker configure

--- bin/configuredb.sh
+++ bin/configuredb.sh
@@ -1,13 +1,27 @@
+# #!/bin/bash
+
+# database="overview"
+
+# echo "Configuring database: $database"
+
+# dropdb -U node_user overview
+# createdb -U node_user overview
+
+# psql -U node_user overview < ./server/database/schema.sql
+
+
+# echo "$database configured"
+
+
 #!/bin/bash
 
 database="overview"
 
 echo "Configuring database: $database"
 
-dropdb -U node_user overview
-createdb -U node_user overview
+docker exec postgresql dropdb -U node_user overview
+docker exec postgresql createdb -U node_user overview
 
-psql -U node_user overview < ./server/database/schema.sql
-
+docker exec -i postgresql psql -U node_user overview < ./server/database/schema.sql
 
 echo "$database configured"
