b9c39d6c37e48fb7f041c89dca4c540b5dbf6ea4
Amelia LI <jkramelia8872@gmail.com>
2023-09-06 00:01:45 -0400

    seeding photos data docker

--- server/database/largeseeder.sql
+++ server/database/largeseeder.sql
@@ -11,14 +11,14 @@
 -- \copy related FROM './largedata/related.csv' DELIMITER ',' CSV HEADER;
 
 
-\copy products FROM '/home/ubuntu/largedata/product.csv' DELIMITER ',' CSV HEADER;
+-- \copy products FROM '/home/ubuntu/largedata/product.csv' DELIMITER ',' CSV HEADER;
 
-\copy features FROM '/home/ubuntu/largedata/features.csv' DELIMITER ',' CSV HEADER;
+-- \copy features FROM '/home/ubuntu/largedata/features.csv' DELIMITER ',' CSV HEADER;
 
-\copy styles FROM '/home/ubuntu/largedata/styles.csv' DELIMITER ',' CSV HEADER;
+-- \copy styles FROM '/home/ubuntu/largedata/styles.csv' DELIMITER ',' CSV HEADER;
 
 \copy photos FROM '/home/ubuntu/largedata/photos.csv' DELIMITER ',' CSV HEADER;
 
-\copy stock FROM '/home/ubuntu/largedata/skus.csv' DELIMITER ',' CSV HEADER;
+-- \copy stock FROM '/home/ubuntu/largedata/skus.csv' DELIMITER ',' CSV HEADER;
 
-\copy related FROM '/home/ubuntu/largedata/related.csv' DELIMITER ',' CSV HEADER;
+-- \copy related FROM '/home/ubuntu/largedata/related.csv' DELIMITER ',' CSV HEADER;
