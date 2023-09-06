d17b6d541db81cc5bdbaa28f3c0f3f74464e4b9d
Amelia LI <jkramelia8872@gmail.com>
2023-09-05 23:29:42 -0400

    deploy seeder

--- server/database/largeseeder.sql
+++ server/database/largeseeder.sql
@@ -1,11 +1,24 @@
-\copy products FROM './largedata/product.csv' DELIMITER ',' CSV HEADER;
+-- \copy products FROM './largedata/product.csv' DELIMITER ',' CSV HEADER;
 
-\copy features FROM './largedata/features.csv' DELIMITER ',' CSV HEADER;
+-- \copy features FROM './largedata/features.csv' DELIMITER ',' CSV HEADER;
 
-\copy styles FROM './largedata/styles.csv' DELIMITER ',' CSV HEADER;
+-- \copy styles FROM './largedata/styles.csv' DELIMITER ',' CSV HEADER;
 
-\copy photos FROM './largedata/photos.csv' DELIMITER ',' CSV HEADER;
+-- \copy photos FROM './largedata/photos.csv' DELIMITER ',' CSV HEADER;
 
-\copy stock FROM './largedata/skus.csv' DELIMITER ',' CSV HEADER;
+-- \copy stock FROM './largedata/skus.csv' DELIMITER ',' CSV HEADER;
 
-\copy related FROM './largedata/related.csv' DELIMITER ',' CSV HEADER;
+-- \copy related FROM './largedata/related.csv' DELIMITER ',' CSV HEADER;
+
+
+\copy products FROM '/home/ubuntu/largedata/product.csv' DELIMITER ',' CSV HEADER;
+
+\copy features FROM '/home/ubuntu/largedata/features.csv' DELIMITER ',' CSV HEADER;
+
+\copy styles FROM '/home/ubuntu/largedata/styles.csv' DELIMITER ',' CSV HEADER;
+
+\copy photos FROM '/home/ubuntu/largedata/photos.csv' DELIMITER ',' CSV HEADER;
+
+\copy stock FROM '/home/ubuntu/largedata/skus.csv' DELIMITER ',' CSV HEADER;
+
+\copy related FROM '/home/ubuntu/largedata/related.csv' DELIMITER ',' CSV HEADER;
